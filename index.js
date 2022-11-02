2cd804704c5acf39aec82c552523c275f66fafb1
xtalkportaldev <mirkobosetti1998@gmail.com>
2022-11-02 22:13:25 +0100

    init, copied from yt

---
+++ index.js
@@ -0,0 +1,25 @@
+window.addEventListener('load', () => {
+	const canvas = document.getElementById('canvas1')
+	const ctx = canvas.getContext('2d')
+
+	canvas.width = window.innerWidth
+	canvas.height = window.innerHeight
+
+	const effect = new Effect(canvas.width, canvas.height)
+	effect.init(ctx)
+
+
+	const animate = () => {
+		ctx.clearRect(0, 0, canvas.width, canvas.height)
+		effect.draw(ctx)
+		effect.update(ctx)
+		requestAnimationFrame(animate)
+	}
+
+	animate()
+
+	const warpButton = document.getElementById('warpButton')
+	warpButton.addEventListener('click', () => {
+		effect.warp()
+	})
+})
---
+++ js/effect.js
@@ -0,0 +1,65 @@
+class Effect {
+	constructor(width, height) {
+		this.width = width
+		this.height = height
+		this.particles = []
+		this.image = document.getElementById('image1')
+		this.centerX = (this.width - this.image.width) / 2
+		this.centerY = (this.height - this.image.height) / 2
+		this.gap = 3
+		this.mouse = {
+			radius: 500,
+			x: null,
+			y: null,
+		}
+
+		window.addEventListener('mousemove', e => {
+			this.mouse.x = e.x
+			this.mouse.y = e.y
+		})
+	}
+
+	init(ctx) {
+		// for (let i = 0; i < 100; i++) {
+		// 	this.particles.push(new Particle(this))
+		// }
+
+		ctx.drawImage(this.image, this.centerX, this.centerY)
+		const pixels = ctx.getImageData(0, 0, this.width, this.height).data
+
+		for (let y = 0; y < this.width; y += this.gap) {
+			for (let x = 0; x < this.height; x += this.gap) {
+				const index = (y * this.width + x) * 4
+
+				const red = pixels[index]
+				const green = pixels[index + 1]
+				const blue = pixels[index + 2]
+				const alpha = pixels[index + 3]
+
+				const color = `rgb(${red},${green},${blue})`
+
+				if (alpha > 0) {
+					this.particles.push(new Particle(this, x, y, color))
+				}
+			}
+		}
+	}
+
+	update() {
+		this.particles.forEach(particle => {
+			particle.update()
+		})
+	}
+
+	draw(ctx) {
+		this.particles.forEach(particle => {
+			particle.draw(ctx)
+		})
+	}
+
+	warp() {
+		this.particles.forEach(particle => {
+			particle.warp()
+		})
+	}
+}
